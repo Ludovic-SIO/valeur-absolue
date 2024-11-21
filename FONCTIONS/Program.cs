67d0109dbbdd3f5773de3e610c2b816762c18b39
ludovic <ludovic@DESKTOP-MQSNRHN>
2024-11-21 19:55:35 +0100

    Ajoutez des fichiers projet.

---
+++ FONCTIONS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FONCTIONS
+{
+    internal class Program
+    {
+        static int absolue (int a)
+        {
+            if (a < 0) return -a;
+            
+           else return a;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a;
+            
+            Console.WriteLine("Mets un nombre entier");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("le nombre est {0}", a);
+
+            Console.WriteLine("La valeur absolue est {0}",absolue(a));
+        }
+    }
+}
